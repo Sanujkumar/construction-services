ba8bfebd3205c688737c3c51967903660aeda658
sanuj <sanujkumar67943@gmail.com>
2024-12-18 23:14:37 +0530

    create searchbar

--- frontend/src/navbarComponents/searchBar.jsx
+++ frontend/src/navbarComponents/searchBar.jsx
@@ -1,10 +1,19 @@
 
-const SearchBar = () => {
-    return(
-        <div className="">  
-            <input placeholder="serch services"/>  
-        </div>
-    );
-}
+import { IoIosSearch } from "react-icons/io";
 
-export default SearchBar;    
+const SearchBar = () => {
+  return (
+    <div className="relative h-10 w-72"> 
+      <input
+        className="rounded-3xl h-full w-full pl-4 pr-10 text-center border focus:outline-none"
+        placeholder="Search services"
+      />
+      <button className="absolute right-2 top-1/2 transform -translate-y-1/2">
+        <IoIosSearch className="h-6 w-6 text-gray-500 hover:text-green-600" />
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;   
+  
